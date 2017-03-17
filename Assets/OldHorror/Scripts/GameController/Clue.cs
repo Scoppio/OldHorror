5313191742d5cc9249be255a6d8fa0e940f67bca
Lucas Coppio <lucascoppio@gmail.com>
2017-03-17 01:41:17 -0300

    starting work with clue list/generator and designing clues manager

---
+++ Assets/OldHorror/Scripts/GameController/Clue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue {
+
+	private int id;
+	private string name;
+	private string description;
+	private List<string> tag;
+
+	public Clue() {}
+
+	public int Id 
+	{
+		get { return this.id; }
+		set { this.id = value; }
+	}
+
+	public string Name
+	{
+		get { return this.name; }
+		set { this.name = value; }
+	}
+
+	public string Description
+	{
+		get { return this.description; }
+		set { this.description = value; }
+	}
+
+	public List<string> Tag 
+	{
+		get { return this.tag; }
+		set { this.tag = value; }
+	}
+
+	public void AddTag (string value) 
+	{
+		this.tag.Add (value);
+	}
+
+	public void RemoveTag (string value)
+	{
+		this.tag.Remove (value);
+	}
+}
---
+++ Assets/OldHorror/Scripts/GameController/CluesList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CluesList : MonoBehaviour {
+
+	private List<Clue> clues;
+
+	// Use this for initialization
+	void Start () {
+		//create N scenarios
+		//Select monster, cults and conspiracys
+		//create n clues for each scenario.
+		//Populate clues, main, secondary and conflicting clues
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
